e2d457ebf04ba0f23e5b98fca8831b1c6963d83a
Fuyang Liu <liufuyang@users.noreply.github.com>
2021-01-16 18:44:39 +0100

    Fix up

--- badger_test.go
+++ badger_test.go
@@ -1,32 +1,65 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"github.com/pingcap/badger"
 	"log"
 	"math"
 	"math/rand"
 	"testing"
+	"unsafe"
 )
 
-func BenchmarkBadger(b *testing.B) {
+var (
+	db  *badger.DB
+	err error
+)
+
+func init() {
 	opts := badger.DefaultOptions
 	opts.Dir = dir
 	opts.ValueDir = valueDir
-	db, err := badger.Open(opts)
+	opts.TableBuilderOptions.BlockSize = 1024
+	opts.TableBuilderOptions.MaxTableSize = 8 << 20 * 4
+	opts.LevelOneSize = 128 << 20
+	opts.TableBuilderOptions.LevelSizeMultiplier = 2
+	db, err = badger.Open(opts)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	for n := 0; n < 20; n++ {
+		get(db, uint32(n))
+	}
+}
 
+func BenchmarkBadger(b *testing.B) {
 	nFound := 0
 	maxValPow1P2 := uint32(math.Pow(float64(MAX_VALUE), 1.2))
 	for n := 0; n < b.N; n++ {
 		key := rand.Uint32() % maxValPow1P2
 		found := get(db, key)
 		if found {
-			nFound ++
+			nFound++
 		}
 	}
-	fmt.Printf("-------> Hit rate: %f\n", float64(nFound) / float64(b.N))
+	fmt.Printf("-------> Hit rate: %f\n", float64(nFound)/float64(b.N))
+}
+
+func Test1(t *testing.T) {
+	var x uint32 = 257
+	bs := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, x)
+	fmt.Println(bs)
+
+	p := unsafe.Pointer(&bs[0])
+	fmt.Println(p)
+	keyAsUint32 := *(*uint32)(p)
+	fmt.Println(keyAsUint32)
+}
+
+func Test2(t *testing.T) {
+	// Trigger get once before
+	get(db, 1234)
+	get(db, 4321)
 }
